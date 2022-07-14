0a4d59f77751ab615526548d630a6042134c8de0
Ghilain <ishimweghilain5050@gmail.com>
2022-07-14 13:06:37 +0200

    Display data from api on homepage and detailspage

--- src/components/header.js
+++ src/components/header.js
@@ -5,30 +5,27 @@ import { BsMic } from 'react-icons/bs';
 import { AiOutlineSetting } from 'react-icons/ai';
 import worldmap from '../image/worldcovidmap.png';
 
-function NavBar() {
+function Header() {
   const currentlocation = useLocation();
   const backHome = currentlocation.pathname.includes('country') ? <MdKeyboardArrowLeft /> : '';
 
   return (
-    <nav>
+    <header>
       <div>
         <NavLink exact="true" to={{ pathname: '/' }}>
           {backHome}
         </NavLink>
-        <h1>World highest covid cases</h1>
+        <h1>Countries has highest covid cases</h1>
         <div>
           <BsMic />
           <AiOutlineSetting />
         </div>
       </div>
       <div>
-        <img src={worldmap} alt="world map"/>
-        <h1>
-          COVID WORLD MAP
-        </h1>
+        <img src={worldmap} alt="world map" />
       </div>
-    </nav>
+    </header>
   );
 }
 
-export default NavBar;
+export default Header;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { BiRightArrowCircle } from 'react-icons/bi';
+import FetchData from '../redux/fetchapidata';
+import { GetData } from '../redux/appreducer';
+
+function HomePage() {
+  const CountryData = useSelector((fetch) => fetch.moredetails);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (CountryData.length === 0) {
+      FetchData()
+        .then((response) => dispatch(GetData(response)));
+    }
+  }, []);
+
+  let CountryHighCases = CountryData.filter((item) => item.country_cases > 10000000);
+  const currentlocation = useLocation();
+  const params = new URLSearchParams(currentlocation.search);
+  const search = params.get('search') || '';
+  CountryHighCases = CountryHighCases.filter((countries) => countries.country_name.toString().split('').join('')
+    .toUpperCase()
+    .toLowerCase()
+    .replace(/\s/g, '')
+    .includes(search));
+  const navigation = useNavigate();
+  const [searchData, setSearchData] = useState(search);
+
+  const countryOnChange = (e) => {
+    navigation(e.target.value ? `?search=${e.target.value}` : '');
+    setSearchData(e.target.value);
+  };
+
+  return (
+    <section>
+      <h1>Countries with high cases</h1>
+      <form>
+        <input
+          type="text"
+          value={searchData}
+          placeholder="Search for country..."
+          onChange={countryOnChange}
+        />
+      </form>
+      <ul>
+        {
+        CountryHighCases.map((countries) => (
+          <li key={countries.country_name}>
+            <Link key={countries.country_name} to={{ pathname: `/country/${countries.country_name}` }}>
+              <BiRightArrowCircle />
+            </Link>
+            <div>
+              <h3>
+                Country:
+                <br />
+                {countries.country_name}
+              </h3>
+              <h3>
+                Continent:
+                <br />
+                {countries.country_continent}
+              </h3>
+              <h3>
+                Population:
+                <br />
+                {countries.population.toLocaleString()}
+              </h3>
+            </div>
+            <div>
+              <img src={countries.country_flag} alt="country flag" />
+            </div>
+          </li>
+        ))
+      }
+      </ul>
+    </section>
+  );
+}
+
+export default HomePage;
